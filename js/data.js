8ffcc4c5017a849edab03a0eaed44ae2288e4e20
DKukushkin91 <Jame7on@gmail.com>
2020-11-04 23:59:12 +0500

    Поправки

--- js/data.js
+++ js/data.js
@@ -6,15 +6,30 @@
     OK: 200
   };
   const TIMEOUT_IN_MS = 10000;
-  const errorTemplate = document.querySelector(`#error`)
-  .content
-  .querySelector(`.error`);
+  // const errorTemplate = document.querySelector(`#error`)
+  // .content
+  // .querySelector(`.error`);
+  // const errorElement = errorTemplate.cloneNode(true);
 
   const showErrorElement = (text) => {
-    const errorElement = errorTemplate.cloneNode(true);
-    errorElement
-                .querySelector(`.error__message`)
-                .textContent = text;
+    const errorElement = document.createElement(`div`);
+    errorElement.style = `
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 800px;
+      height: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 15px;
+      background-color: rgba(0, 0, 0, 0.9);
+      color: #fff;
+      font-weight: 700;
+      line-height: 1.5;
+      z-index: 100`;
+    errorElement.textContent = text;
     document
             .querySelector(`.map`)
             .append(errorElement);
@@ -35,7 +50,7 @@
     xhr.addEventListener(`error`, () => {
       onError(showErrorElement(`Произошла ошибка соединения`));
     });
-    xhr.addEventListener(`timeout`, function () {
+    xhr.addEventListener(`timeout`, () => {
       onError(showErrorElement(`Запрос не успел выполниться за ${xhr.timeout} мс`));
     });
 
