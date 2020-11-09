3a0c5e2c3529e42c8920277a089b5e8c90768554
DKukushkin91 <Jame7on@gmail.com>
2020-11-10 00:45:56 +0500

    Убрал лишнюю переменную

--- js/filter.js
+++ js/filter.js
@@ -2,15 +2,11 @@
 
 (() => {
   const mapFilters = document.querySelector(`.map__filters`);
-  const filterHousingType = mapFilters.querySelector(`#housing-type`);
 
-  filterHousingType.addEventListener(`change`, () => {
+  mapFilters.querySelector(`#housing-type`).addEventListener(`change`, () => {
     window.util.onCardRemove();
     window.util.onPinsRemove();
     window.render.onCreatePins();
   });
 
-  window.filter = {
-    filterHousingType
-  };
 })();
