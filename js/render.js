99ec6af8b46e06e36a5e44351431e6645bfea730
DKukushkin91 <Jame7on@gmail.com>
2020-11-10 01:38:51 +0500

    Фильтрация по типу жилья

--- js/render.js
+++ js/render.js
@@ -2,25 +2,47 @@
 
 (() => {
   const pinListElement = document.querySelector(`.map__pins`);
+  const mapFilters = document.querySelector(`.map__filters`);
   const filterHousingType = document.querySelector(`#housing-type`);
+  const MAX_PIN = 5;
+  const anyValue = `any`;
 
   let load = [];
   window.load.dataRetrivial((data) => {
     load = data;
   });
 
+  const onFilterPins = (arr) => {
+    return arr.slice(0, MAX_PIN);
+  };
+
+  const onFilterHousingType = (element) => {
+    return element.offer.type === filterHousingType.value || filterHousingType.value === anyValue;
+  };
+
+  const onFilterMapAd = () => {
+    return onFilterPins(load.filter((ad) => {
+      return onFilterHousingType(ad);
+    }));
+  };
+
   const onCreatePins = () => {
     const fragmentPin = document.createDocumentFragment();
-    const housingType = load.filter((element) => {
-      return element.offer.type === filterHousingType.value || filterHousingType.value === `any`;
-    }).slice(0, 5);
-    for (let pin of housingType) {
+    onFilterMapAd();
+    for (let pin of onFilterMapAd()) {
       const pinElement = window.pins.getRenderPin(pin);
       window.card.onPinOpenCard(pinElement, pin);
       fragmentPin.appendChild(pinElement);
     }
     window.util.createPin(pinListElement, fragmentPin);
   };
+
+  mapFilters.addEventListener(`change`, () => {
+    window.util.onPinsRemove();
+    window.util.onCardRemove();
+    onCreatePins();
+  });
+
 
   window.render = {
     onCreatePins,
